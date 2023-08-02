511e05f3fb91f1b62ff125146157a73661f273bb
wugx <wugx@qq.com>
2023-08-02 21:31:48 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Chain Gate Wallet',
+    description: '...',
+    themeConfig: {
+        sidebar: [
+          '/',
+          '/privacy-policy'
+        ],
+        nav: [
+            { text: 'terms-of-service', link: '/' },
+            { text: 'privacy-policy', link: '/privacy-policy' },
+        ]
+    }
+}
